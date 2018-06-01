d5c42359c4a89494e0fdc1122281ee4ca8cdb48d
Vova <volodfast@gmail.com>
2018-06-01 20:01:12 +0300

    Finish user sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,23 @@ class UsersController < ApplicationController
     end
 
     def create
-
+        @user = User.create(user_params)
+        if @user.save
+            flash[:success] = "Welcome to the Task Management System!"
+            redirect_to @user
+        else
+            render 'new'
+        end
     end
 
     def new
         @user = User.new
     end
+
+    private 
+
+        def user_params
+            params.require(:user).permit(:first_name, :last_name, 
+                                        :email, :password, :password_confirmation)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/help', to: 'static_pages#help'
+  get '/signup', to: 'users#new'
+
+  resources :users
 end
